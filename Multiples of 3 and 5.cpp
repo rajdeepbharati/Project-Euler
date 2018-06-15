ca4af69ffd8c11bbfba2ae798483ef9e35858c74
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-06-15 17:49:01 +0530

    Create Multiples of 3 and 5.cpp

---
+++ Multiples of 3 and 5.cpp
@@ -0,0 +1,15 @@
+// https://projecteuler.net/problem=1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
